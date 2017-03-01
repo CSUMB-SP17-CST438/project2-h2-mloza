91e859d3785c7017972324d0d8305e10161965a9
Maria Loza <mloza@csumb.edu>
2017-03-01 22:40:06 +0000

    cleaned

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -12,15 +12,11 @@ class ServerIntegrationTestCase(flask_testing.LiveServerTestCase):
 
     def test_server_is_running(self):
         r = urllib2.urlopen(self.get_server_url())
-        # print r
         self.assertEqual(r.code, 200)
-        # self.assertEquals(r.text, 'Hello, world!')
         
     def test_server_render(self):
         r = requests.get(self.get_server_url()) 
-        # print r.headers['content-type']
         self.assertEquals(r.headers['content-type'], 'text/html; charset=utf-8')
-        # self.assert_template_used('index.html')
 
 
 
