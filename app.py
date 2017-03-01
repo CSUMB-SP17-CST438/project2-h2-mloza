70a36f5f58053c669e17da267e9254504a47bdae
Maria Loza <mloza@csumb.edu>
2017-03-01 06:08:42 +0000

    chat can now search for different food in static location

--- app.py
+++ app.py
@@ -169,8 +169,37 @@ def on_new_chat(data):
     
     if data['chat'].find("!!", 0, 2) != -1:
         # print " DRAGON "
+        print data['chat']
         if data['chat'].find("!! say", 0, 6) != -1:
             botChat = data['chat'][7:]
+        elif data['chat'].find("!! food", 0, 7) != -1:
+            food = data['chat'][8:]
+            url = "https://api.yelp.com/v2/search?term="+food+"&location=Monterey, CA&limit=40" 
+            oauth = requests_oauthlib.OAuth1(
+                'P9HpnKvg3flN6o1KrsHgxw', 
+                'uJm6epI8CUT968OQc_8K0xBR9PQ',
+                'eCEh6_yEE7S1i0r3h7GNO-CIF_llaDOb',
+                'V_SBhCGKhBruxND3mswQC1pYCYE'
+            )
+            response = requests.get(url, auth=oauth)
+            json_body = response.json()
+            randNum = random.randint(0, 39) #only 40 hits are returned
+            name = json_body["businesses"][randNum]["name"]
+            yelpURL = json_body["businesses"][randNum]["url"]
+            phone = json_body["businesses"][randNum]["display_phone"]
+            foodStreet = json_body["businesses"][randNum]["location"]["display_address"][0]
+            foodCity = json_body["businesses"][randNum]["location"]["display_address"][1]
+            botChat = yelpURL
+            all_chats.append({        
+                'name': 'Dragon-bot',        
+                'picture': chatBotImg,        
+                'chat': botChat,   
+                'url': 'U',
+            })
+            msg = models.Message(chatBotImg, '1', 'Dragon-bot', botChat, 'U')
+            models.db.session.add(msg)
+            models.db.session.commit()
+            botChat = name + " " + foodStreet + " " + foodCity + " " + phone
         else:
             if data['chat'] == "!! about":
                 botChat = "Hello! I am Dragon-bot. This is a fun place to chat."
@@ -180,36 +209,36 @@ def on_new_chat(data):
                 botChat = "RAAAAWR!!!"
             elif data['chat'] == "!! eat":
                 botChat = "FREE FOOD! Thank you!"
-            elif data['chat'] == "!! food":
-                botChat = "coming soon"
-                url = "https://api.yelp.com/v2/search?term=sushi&location=Monterey, CA&limit=40" 
-                oauth = requests_oauthlib.OAuth1(
-                    'P9HpnKvg3flN6o1KrsHgxw', 
-                    'uJm6epI8CUT968OQc_8K0xBR9PQ',
-                    'eCEh6_yEE7S1i0r3h7GNO-CIF_llaDOb',
-                    'V_SBhCGKhBruxND3mswQC1pYCYE'
-                )
-                response = requests.get(url, auth=oauth)
-                json_body = response.json()
-                randNum = random.randint(0, 39) #only 40 hits are returned
-                name = json_body["businesses"][randNum]["name"]
-                yelpURL = json_body["businesses"][randNum]["url"]
-                phone = json_body["businesses"][randNum]["display_phone"]
-                foodStreet = json_body["businesses"][randNum]["location"]["display_address"][0]
-                foodCity = json_body["businesses"][randNum]["location"]["display_address"][1]
-                botChat = yelpURL
-                all_chats.append({        
-                    'name': 'Dragon-bot',        
-                    'picture': chatBotImg,        
-                    'chat': botChat,   
-                    'url': 'U',
-                })
-                msg = models.Message(chatBotImg, '1', 'Dragon-bot', botChat, 'U')
-                models.db.session.add(msg)
-                models.db.session.commit()
-                botChat = name + " " + foodStreet + " " + foodCity + " " + phone
-                # print json_body
-                # print "LOCATION: " + foodStreet + " " + foodCity
+            # elif data['chat'] == "!! food":
+            #     botChat = "coming soon"
+            #     url = "https://api.yelp.com/v2/search?term=sushi&location=Monterey, CA&limit=40" 
+            #     oauth = requests_oauthlib.OAuth1(
+            #         'P9HpnKvg3flN6o1KrsHgxw', 
+            #         'uJm6epI8CUT968OQc_8K0xBR9PQ',
+            #         'eCEh6_yEE7S1i0r3h7GNO-CIF_llaDOb',
+            #         'V_SBhCGKhBruxND3mswQC1pYCYE'
+            #     )
+            #     response = requests.get(url, auth=oauth)
+            #     json_body = response.json()
+            #     randNum = random.randint(0, 39) #only 40 hits are returned
+            #     name = json_body["businesses"][randNum]["name"]
+            #     yelpURL = json_body["businesses"][randNum]["url"]
+            #     phone = json_body["businesses"][randNum]["display_phone"]
+            #     foodStreet = json_body["businesses"][randNum]["location"]["display_address"][0]
+            #     foodCity = json_body["businesses"][randNum]["location"]["display_address"][1]
+            #     botChat = yelpURL
+            #     all_chats.append({        
+            #         'name': 'Dragon-bot',        
+            #         'picture': chatBotImg,        
+            #         'chat': botChat,   
+            #         'url': 'U',
+            #     })
+            #     msg = models.Message(chatBotImg, '1', 'Dragon-bot', botChat, 'U')
+            #     models.db.session.add(msg)
+            #     models.db.session.commit()
+            #     botChat = name + " " + foodStreet + " " + foodCity + " " + phone
+            #     # print json_body
+            #     # print "LOCATION: " + foodStreet + " " + foodCity
             else:
                 botChat = "Nope! Check out !! help"
         all_chats.append({        
